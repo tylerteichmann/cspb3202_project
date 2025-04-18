a3531df43073c145f91e7f03aa5bf45eed7ffd49
Tyler <tyler.j.teichmann@gmail.com>
2025-04-18 15:19:26 -0600

    Updated to include Q-Learning agent

--- lander.py
+++ lander.py
@@ -9,22 +9,60 @@
 
 
 import gymnasium as gym
-import swig
+from agent import QLanderAgent
+from tqdm import tqdm
+import visualize
 
 
 def main():
-    # Create environment
-    env = gym.make("LunarLander-v3", render_mode="human")
-    env.reset()
 
-    episode_over = False
+    # Environment setup
+    env = gym.make("LunarLander-v3")
 
-    while not episode_over:
-        action = env.action_space.sample()
-        observation, reward, terminated, truncated, info = env.step(action)
 
-        episode_over = terminated or truncated
+    # Agent setup
+    alpha = 0.001
+    n_episodes = 1000000
+    start_epsilon = 1.0
+    epsilon_decay = start_epsilon / (n_episodes / 2)
+    final_epsilon = 0.1
 
+    agent = QLanderAgent(
+        env=env,
+        alpha=alpha,
+        initial_epsilon=start_epsilon,
+        epsilon_decay=epsilon_decay,
+        final_epsilon=final_epsilon,
+    )
+
+
+    env = gym.wrappers.RecordEpisodeStatistics(env, buffer_length=n_episodes)
+
+
+    # Run itterations
+    for i in tqdm(range(n_episodes)):
+        state, _ = env.reset()
+        state = tuple(state)
+        episode_over = False
+
+        while not episode_over:
+            action = agent.get_action(state)
+            state_prime, reward, terminal, truncated, _ = env.step(action)
+            state_prime = tuple(state_prime)
+
+            agent.update_q_values(
+                state=state,
+                action=action,
+                reward=reward,
+                terminal=terminal,
+                state_prime=state_prime
+            )
+
+            state = state_prime
+            episode_over = terminal or truncated
+
+
+    visualize.visualize(env, agent)
     env.close()
 
 
